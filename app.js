bc438b108428ccbb00386540517f8cc87002a5ed
Joe Gibbs <joe.gibbs0@gmail.com>
2020-01-31 00:05:21 +1100

    Added FPS counter

--- app.js
+++ app.js
@@ -126,8 +126,12 @@ class Player {
         this.items.remove(item);
     }
 
+    chop(map) {
+         map[this.x + this.direction[0]][this.y + this.direction[1]].type = terrain.LAND;
+    }
+
     move(x,y, map) {   
-             
+        this.direction = [x, y];
         if (map.tiles[this.x + x][this.y + y].type.walkable) {
             this.x += x;
             this.y += y;
@@ -228,6 +232,8 @@ window.onload = function() {
                 break;
             case 'KeyE':
                 player.pickup(droppedItems);
+            case 'Enter':
+                player.chop(map.tiles);
             default:
                 break;
         }        
@@ -332,7 +338,6 @@ window.onload = function() {
                     {
                         result.push(renderableTiles[x][y-1]);  
                     }        
-                    console.log(result);
                     
                     return result;            
                 }
@@ -384,7 +389,46 @@ window.onload = function() {
         window.requestAnimationFrame(loop);
     }
 
+    const fps = {
+        sampleSize : 60,    
+        value : 0,
+        _sample_ : [],
+        _index_ : 0,
+        _lastTick_: false,
+        tick : function(){
+            // if is first tick, just set tick timestamp and return
+            if( !this._lastTick_ ){
+                this._lastTick_ = performance.now();
+                return 0;
+            }
+            // calculate necessary values to obtain current tick FPS
+            let now = performance.now();
+            let delta = (now - this._lastTick_)/1000;
+            let fps = 1/delta;
+            // add to fps samples, current tick fps value 
+            this._sample_[ this._index_ ] = Math.round(fps);
+            
+            // iterate samples to obtain the average
+            let average = 0;
+            for(let i=0; i<this._sample_.length; i++) average += this._sample_[ i ];
+    
+            average = Math.round( average / this._sample_.length);
+    
+            // set new FPS
+            this.value = average;
+            // store current timestamp
+            this._lastTick_ = now;
+            // increase sample index counter, and reset it
+            // to 0 if exceded maximum sampleSize limit
+            this._index_++;
+            if( this._index_ === this.sampleSize) this._index_ = 0;
+            return this.value;
+        }
+    }    
+
     function loop() {
+        let fpsValue = fps.tick();
+        window.fps.innerHTML = fpsValue;
         update();
         draw();
     }
