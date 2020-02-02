7c029e84831d66eb35c7dbddd50b4f36f5d4502b
Samuel Losada <samthesam26@gmail.com>
2020-02-02 13:38:14 +1100

    added sand tiles, made terrain tiles to get an array of images from init, that way we can look into varying the tiles and maybe even animate them.

--- src/terrain.js
+++ src/terrain.js
@@ -1,9 +1,9 @@
 import items from './items.js';
 
 class Terrain {
-    constructor(name, walkable, health, image, drops) {
+    constructor(name, walkable, health, images, drops) {
         this.tile = new Image();
-        this.tile.src = image;
+        this.tile.src = images;
         this.name = name;
         this.walkable = walkable;
         this.drops = drops;
@@ -12,11 +12,11 @@ class Terrain {
 }
 
 const terrain = {
-    "LAND": new Terrain('Land', true, Infinity, './assets/img/grass_2.png', []),
-    "SAND": new Terrain('Sand', true, Infinity, './assets/img/sand.png', []),
-    "WATER": new Terrain('Water', false, Infinity, './assets/img/water.png', []),
-    "ROCK": new Terrain('Rock', false, 500, './assets/img/rock.png',[items.gold, items.stone]),
-    "FOREST": new Terrain('Forest', false, 50, './assets/img/forest.png', [items.wood]),
+    "LAND": new Terrain('Land', true, Infinity, ['./assets/img/grass_1.png'], []),
+    "SAND": new Terrain('Sand', true, Infinity, ['./assets/img/sand.png'], []),
+    "WATER": new Terrain('Water', false, Infinity, ['./assets/img/water.png'], []),
+    "ROCK": new Terrain('Rock', false, 500, ['./assets/img/rock.png'],[items.gold, items.stone]),
+    "FOREST": new Terrain('Forest', false, 50, ['./assets/img/forest.png'], [items.wood]),
 }
 
 Object.freeze(terrain);
