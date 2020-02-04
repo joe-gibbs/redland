1b1931592dacf8244eaa96520369bdae4bc69de1
Joe Gibbs <joe.gibbs0@gmail.com>
2020-02-04 15:35:36 +1100

    Fixed movement issues and smoothed scrolling

--- src/classes/player.js
+++ src/classes/player.js
@@ -18,6 +18,7 @@ export default class Player {
         this.items = [];
         this.health = 100;
         this.direction = [0,1];
+        this.movementAxis = [0,0];
         this.closestX = x;
         this.closestY = y;
         this.aimedTile; //Tile that the player is looking at based on direction.
@@ -25,6 +26,40 @@ export default class Player {
 
     updateAimedTile(map){
         this.aimedTile = map.tiles[Math.round(this.x + this.direction[0])][Math.round(this.y + this.direction[1])];
+    }
+
+    updateMovement(map) { 
+        let x = this.movementAxis[0];
+        let y = this.movementAxis[1];
+
+        if (map.tiles[Math.round(this.x + x)][Math.round(this.y + y)].type.walkable) {
+            this.x += x;
+            this.y += y;
+        }
+        else if (map.tiles[Math.round(this.x + x)][Math.round(this.y)].type.walkable) {
+            this.x += x;
+        }
+        else if (map.tiles[Math.round(this.x)][Math.round(this.y + y)].type.walkable) {
+            this.y += y;
+        }
+
+        x = x * 0.8;
+        y = y * 0.8;
+
+        if (x < 0.03 && x > -0.03) {
+            x = 0;
+        }
+
+        if (y < 0.03 && y > -0.03) {
+            y = 0;
+        }
+
+        this.closestX = Math.round(this.x);
+        this.closestY = Math.round(this.y);    
+
+        this.updateAimedTile(map)
+
+        this.movementAxis = [x, y];
     }
 
     pickup(droppedItems) {
@@ -74,16 +109,15 @@ export default class Player {
 
     move(x,y, map) { 
         this.direction = [x, y];
-            
-        if (map.tiles[Math.round(this.x + x)][Math.round(this.y + y)].type.walkable) {
-            this.x += x;
-            this.y += y;
-        }
+                     
+        this.movementAxis[0] += x;
+        this.movementAxis[1] += y;
 
-        this.closestX = Math.round(this.x);
-        this.closestY = Math.round(this.y);    
-         
-        this.updateAimedTile(map);
+        this.movementAxis[0] = this.movementAxis[0].clamp(-0.12, 0.12);
+        this.movementAxis[1] = this.movementAxis[1].clamp(-0.12, 0.12);
+
+        console.log(this.movementAxis);
+
         return map.tiles[this.closestX][this.closestY];
     }
 }
