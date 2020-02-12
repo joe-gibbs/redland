90024ce7a40bdd216a474cebd1b53c8c23d409ea
Samuel Losada <samthesam26@gmail.com>
2020-02-12 22:01:58 +1100

    new rocks and logs

--- src/items.js
+++ src/items.js
@@ -6,8 +6,8 @@ const items = {
     "pick": new Item('Pick', './assets/img/pick.png'),
     "shovel": new Item('Shovel', './assets/img/shovel.png'),
     "wood": new Item('Wood', './assets/img/logs.png'),
-    "gold": new Item('Gold', './assets/img/logs.png'),
-    "stone" : new Item('Stone', './assets/img/logs.png'),
+    "gold": new Item('Gold', './assets/img/rocks.png'),
+    "stone" : new Item('Stone', './assets/img/rocks.png'),
     "food": new Item('Food', './assets/img/logs.png'),
     "boat": new Item('Boat', './assets/img/raft.png'),
     "treasure": new Treasure('Treasure')
