7b86dd4978309f0626693e5e5dfd1eb06e6a3702
Joe Gibbs <joe.gibbs0@gmail.com>
2020-02-13 11:06:13 +1100

    Fixed drawing stone on resources

--- src/items.js
+++ src/items.js
@@ -2,16 +2,16 @@ import Item from './classes/item.js';
 import Treasure from './classes/treasure.js';
 
 const items = {
-    "axe": new Item('Axe', './assets/img/axe.png'),
-    "pick": new Item('Pick', './assets/img/pick.png'),
-    "shovel": new Item('Shovel', './assets/img/shovel.png'),
-    "wood": new Item('Wood', './assets/img/logs.png'),
-    "gold": new Item('Gold', './assets/img/rocks.png'),
-    "stone" : new Item('Stone', './assets/img/rocks.png'),
-    "food": new Item('Food', './assets/img/logs.png'),
-    "boat": new Item('Boat', './assets/img/raft.png'),
-    "map": new Item('Map', './assets/img/world_map.png'),
-    "treasure": new Treasure('Treasure')
+    axe: new Item('Axe', './assets/img/axe.png'),
+    pick: new Item('Pick', './assets/img/pick.png'),
+    shovel: new Item('Shovel', './assets/img/shovel.png'),
+    wood: new Item('Wood', './assets/img/logs.png'),
+    gold: new Item('Gold', './assets/img/rocks.png'),
+    stone: new Item('Stone', './assets/img/rocks.png'),
+    food: new Item('Food', './assets/img/logs.png'),
+    boat: new Item('Boat', './assets/img/raft.png'),
+    map: new Item('Map', './assets/img/world_map.png'),
+    treasure: new Treasure('Treasure')
 }
 
 Object.freeze(items);
