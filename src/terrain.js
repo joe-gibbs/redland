dd89bb0c17bf5d78da67c01b08e044886d349abc
Joe Gibbs <joe.gibbs0@gmail.com>
2020-02-19 13:55:15 +1100

    Added grass variation

--- src/terrain.js
+++ src/terrain.js
@@ -23,6 +23,7 @@ class Terrain {
 
 const terrain = {
     "LAND": new Terrain('Land', true, false, 50, ['./assets/img/grass_2.png'],[]),
+    "TALL_GRASS": new Terrain('Tall Grass', true, false, 50, ['./assets/img/grass_1.png'], []),
     "SAND": new Terrain('Sand', true, false, 50, ['./assets/img/sand.png'], []),
     "WATER": new Terrain('Water', false, true, Infinity, ['./assets/img/water.png'], []),
     "ROCK": new Terrain('Rock', false, false, 500, ['./assets/img/rock.png'],[items.stone]),
@@ -32,5 +33,4 @@ const terrain = {
 }
 
 Object.freeze(terrain);
-
 export default terrain;
