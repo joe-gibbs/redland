94adea031296ffe463f571b53b06a73835d62a50
Joe Gibbs <joe.gibbs0@gmail.com>
2020-02-20 11:20:34 +1100

    Removed FPS counter

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ import DroppedItem from './src/classes/droppedItem.js';
 import GameMap from './src/classes/gameMap.js';
 import Player from './src/classes/player.js';
 import MapGenerator from './src/classes/mapGenerator.js';
-import fps from './src/fpsCounter.js';
 import MapRenderer from './src/classes/mapRenderer.js';
 import items from './src/items.js';
 import UiRenderer from './src/classes/uiRenderer.js';
@@ -283,8 +282,6 @@ function load() {
     }
 
     function gameLoop() {
-        let fpsValue = fps.tick();
-        window.fps.innerHTML = fpsValue;
         update();
         draw();
         updateTime();
