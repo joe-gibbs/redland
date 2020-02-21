0a4b7bbf9c65b5e9fa2b7ece5b309e04ce079f0c
Joe Gibbs <joe.gibbs0@gmail.com>
2020-02-21 20:14:35 +1100

    Changed log statement

--- app.js
+++ app.js
@@ -210,13 +210,7 @@ function load() {
         });
   
         if ('serviceWorker' in navigator) {            
-            navigator.serviceWorker.register('/sw.js').then(function(registration) {
-                // Registration was successful
-                console.log('ServiceWorker registration successful with scope: ', registration.scope);
-              }, function(err) {
-                // registration failed :(
-                console.log('ServiceWorker registration failed: ', err);
-              });          
+            navigator.serviceWorker.register('/sw.js');
         }
 
         onkeydown = onkeyup = function(e){
